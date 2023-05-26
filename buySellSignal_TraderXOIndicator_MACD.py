31cae38499953fffc39327227a5f3bba15afb2d4
tonlone <tonlone@hotmail.com>
2023-05-26 17:20:52 -0400

    Add files via upload

--- buySellSignal_TraderXOIndicator_MACD.py
+++ buySellSignal_TraderXOIndicator_MACD.py
@@ -4,8 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define the ticker symbols of the stocks you want to analyze
-#ticker_symbols = ['NVDA']
-ticker_symbols = ['COST', 'MSFT', 'PYPL', 'PG', 'UNH', 'V', 'WMT', 'NFLX', 'NVDA', 'CMCSA' ]
+ticker_symbols = ['JPM']
+#ticker_symbols = ['COST', 'MSFT', 'PYPL', 'PG', 'UNH', 'V', 'WMT', 'NFLX', 'NVDA', 'CMCSA' ]
+#ticker_symbols  = ['AAPL', 'ABT', 'ABBV', 'ACN', 'ADBE', 'AMZN', 'BAC', 'BMY', 'CMCSA', 'COST', 'CSCO', 'CRM', 'CVX', 'DHR', 'DIS', 'GOOGL', 'HD', 'HON', 'INTC', 'JNJ', 'JPM', 'KO', 'LIN', 'LLY', 'MA', 'MCD', 'MMM', 'MRK', 'MSFT', 'NEE', 'NFLX', 'NVDA', 'NKE', 'ORCL', 'PFE', 'PEP', 'PG', 'PM', 'PYPL', 'T', 'TMO', 'TSLA', 'UNH', 'UNP', 'V', 'VZ', 'WMT', 'XOM']
+#ticker_symbols = ['BTC-USD', 'ETH-USD', 'DOGE-USD', 'USDT-USD', 'ADA-USD', 'BNB-USD', 'XRP-USD']
+
 start_date = '2022-10-01'
 
 # Define the Trader XO indicator calculation function
